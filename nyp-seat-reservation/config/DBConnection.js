90c1f8b5f09928607eab11a2b4d2b173f4ab0633
lester-chua-2001 <58807104+lester-chua-2001@users.noreply.github.com>
2020-10-01 16:51:15 +0800

    Update DBConnection.js

--- nyp-seat-reservation/config/DBConnection.js
+++ nyp-seat-reservation/config/DBConnection.js
@@ -12,12 +12,46 @@ const EventSeatTypes = require('../../nyp-seat-reservation/models/EventSeatType'
 const setUpDB = (drop) => {
     mySQLDB.authenticate()
     .then(() => {
+    //event helpers & event
+        Events.hasMany(EventHelpers, { foreignKey: 'EventId' });
+        EventHelpers.belongsTo(Events, { foreignKey: 'EventId' });
+        //event attendees & event
+        Events.hasMany(EventAttendees, { foreignKey: 'EventId' });
+        EventAttendees.belongsTo(Events, { foreignKey: 'EventId' });
+        //event reserved seats & event
+        Events.hasMany(EventReservedSeats, { foreignKey: 'EventId' });
+        EventReservedSeats.belongsTo(Events, { foreignKey: 'EventId' });
+        //event seats & event
+        Events.hasMany(EventSeats, { foreignKey: 'EventId' });
+        EventSeats.belongsTo(Events, { foreignKey: 'EventId'});
+        //event seat type & event
+        Events.hasMany(EventSeatTypes, { foreignKey: 'EventId' });
+        EventSeatTypes.belongsTo(Events, { foreignKey: 'EventId' });
+
+
+        //venues & planners
+        Venues.hasMany(Users, { foreignKey: 'UserId' });
+        Users.belongsTo(Venues, { foreignKey: 'UserId' });
+
+        //events seats & events seat type
+        EventSeats.hasMany(EventSeatTypes, { foreignKey: 'SeatTypeId' });
+        EventSeatTypes.belongsTo( EventSeats, { foreignKey: 'SeatTypeId' });
+        
+        //events reserved seats & attendee
+        EventReservedSeats.hasMany(EventAttendees, { foreignKey: 'AttendeeId' });
+        EventAttendees.belongsTo(EventReservedSeats, { foreignKey: 'AttendeeId' });
+        //events reserved seats & seat type
+        EventReservedSeats.hasMany(EventSeatTypes, { foreignKey: 'SeatTypeId' });
+        EventSeatTypes.belongsTo(EventReservedSeats, { foreignKey: 'SeatTypeId' });
+
+        //user & event helpers
+        Users.hasMany(EventHelpers, { foreignKey: 'UserId' });
+        EventHelpers.belongsTo(Users, { foreignKey: 'UserId' });
+
         mySQLDB.sync({ force: drop })
 
     })
 };
-
-
 
 module.exports = {
     setUpDB
