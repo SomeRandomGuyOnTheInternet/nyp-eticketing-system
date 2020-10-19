b14ca16c55fce2fdb8d5d99a3c5d279ba6d40146
lester-chua-2001 <58807104+lester-chua-2001@users.noreply.github.com>
2020-10-19 16:24:24 +0800

    SMS

--- nyp-seat-reservation/public/js/ajax.js
+++ nyp-seat-reservation/public/js/ajax.js
@@ -129,18 +129,29 @@ createEventHelpers = async (eventHelperArray) => {
     });
 };
 
+sendSMS = async (number, message) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await promiseAjax(
+                'https://sms.sit.nyp.edu.sg/SMSWebService/sms.asmx/sendMessage', 
+                'POST',
+                `SMSAccount=FYPJ01&Pwd=529287&Mobile=${number}&Message=${message}`,
+                'jsonp', 
+                'application/x-www-form-urlencoded');
+            resolve(res);
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
 
 
 
 callTestApi = async () => {
     return new Promise(async (resolve, reject) => {
         try {
-            const res = await promiseAjax(
-                'https://sms.sit.nyp.edu.sg/SMSWebService/sms.asmx/sendMessage', 
-                'POST',
-                `SMSAccount=FYPJ01&Pwd=529287&Mobile=91864675&Message=innocent  message`,
-                'jsonp', 
-                'application/x-www-form-urlencoded');
+            const res = await promiseAjax(`${baseRoute}/test`, 'GET', null);
             resolve(res);
         } catch (error) {
             reject(error);
