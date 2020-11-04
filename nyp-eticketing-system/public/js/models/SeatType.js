6a62806df365146c48ff2ade074d8b4d99b4ad73
lester-chua-2001 <58807104+lester-chua-2001@users.noreply.github.com>
2020-11-04 13:38:30 +0800

    deleted useless files

--- nyp-eticketing-system/public/js/models/SeatType.js
+++
@@ -1,8 +0,0 @@
-class SeatType {
-    constructor(obj) {
-        this.char = obj.char;
-        this.category = obj.category;
-        this.blocked = obj.blocked;
-        this.classes = obj.classes;
-    }
-}
