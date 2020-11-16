fb42830a127e4890cd8b5ad6403e9fc5306168b4
Vignesh <33924596+SomeRandomGuyOnTheInternet@users.noreply.github.com>
2020-11-16 22:14:21 +0800

    fixed some bugs

--- nyp-eticketing-system/public/js/toast.js
+++ nyp-eticketing-system/public/js/toast.js
@@ -10,22 +10,27 @@ const toast = {
 };
 
 showToast = (notification) => {
-    let visibleToasts = $('.toast').not('.hide');
-    let toastWithSameMessageExists = false;
+    $('.toast').not('.hide').toast('hide'); // Show the unhidden toasts in the toast container, which includes the one we just appended
+    let toastNode = templates.toast(notification); // Upon clicking each toast's buttons, we call pass the template string we defined in template.js and the variables required in the templates to ejs so it gives us a complete html element with all the data filled in
+    $("#toastContainer").append(toastNode); // Then we append the resulting toast html to the toast container defined in footer.ejs
+    $('.toast').not('.hide').toast('show'); // Show the unhidden toasts in the toast container, which includes the one we just appended
 
-    for (let i = 0; i < visibleToasts.length; i++) {
-        if (visibleToasts[i].querySelector('.notification-message').innerText == notification.message) {
-            visibleToasts[i].classList.remove('quickScale');
-            void visibleToasts[i].offsetWidth;
-            visibleToasts[i].classList.add('quickScale');
-            toastWithSameMessageExists = true;
-            break;
-        }
-    }
+    // let visibleToasts = $('.toast').not('.hide');
+    // let toastWithSameMessageExists = false;
 
-    if (toastWithSameMessageExists === false) {
-        let toastNode = templates.toast(notification); // Upon clicking each toast's buttons, we call pass the template string we defined in template.js and the variables required in the templates to ejs so it gives us a complete html element with all the data filled in
-        $("#toastContainer").append(toastNode); // Then we append the resulting toast html to the toast container defined in footer.ejs
-        $('.toast').not('.hide').toast('show'); // Show the unhidden toasts in the toast container, which includes the one we just appended
-    }
+    // for (let i = 0; i < visibleToasts.length; i++) {
+    //     if (visibleToasts[i].querySelector('.notification-message').innerText == notification.message) {
+    //         visibleToasts[i].classList.remove('quickScale');
+    //         void visibleToasts[i].offsetWidth;
+    //         visibleToasts[i].classList.add('quickScale');
+    //         toastWithSameMessageExists = true;
+    //         break;
+    //     }
+    // }
+
+    // if (toastWithSameMessageExists === false) {
+    //     let toastNode = templates.toast(notification); // Upon clicking each toast's buttons, we call pass the template string we defined in template.js and the variables required in the templates to ejs so it gives us a complete html element with all the data filled in
+    //     $("#toastContainer").append(toastNode); // Then we append the resulting toast html to the toast container defined in footer.ejs
+    //     $('.toast').not('.hide').toast('show'); // Show the unhidden toasts in the toast container, which includes the one we just appended
+    // }
 };
